5d9b12f531ad9c8983ca3a871cf0f4f14c7d6b64
wentao <skinwentao@gmail.com>
2017-11-20 16:20:04 +0800

    添加日志

--- src/main/java/com/bstek/urule/console/DefaultRepositoryInteceptor.java
+++ src/main/java/com/bstek/urule/console/DefaultRepositoryInteceptor.java
@@ -5,46 +5,46 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class DefaultRepositoryInteceptor implements RepositoryInteceptor {
-    private static final Logger logger  = LoggerFactory.getLogger(DefaultRepositoryInteceptor.class);
+    private static final Logger logger = LoggerFactory.getLogger(DefaultRepositoryInteceptor.class);
 
     @Override
     public void readFile(String file) {
-
+        logger.info("readFile :{}", file);
     }
 
     @Override
     public void saveFile(String file, String content) {
         // TODO Auto-generated method stub
-
+        logger.info("saveFile : file :{},content :{}", file, content);
     }
 
     @Override
     public void createFile(String file, String content) {
         // TODO Auto-generated method stub
-
+        logger.info("createFile : file :{},content :{}", file, content);
     }
 
     @Override
     public void deleteFile(String file) {
         // TODO Auto-generated method stub
-
+        logger.info("deleteFile: file:{}", file);
     }
 
     @Override
     public void renameFile(String oldFileName, String newFileName) {
         // TODO Auto-generated method stub
-
+        logger.info("renameFile : oldFileName:{} , newFileName", oldFileName, newFileName);
     }
 
     @Override
     public void createDir(String dir) {
         // TODO Auto-generated method stub
-
+        logger.info("createDir dir:{}", dir);
     }
 
     @Override
     public void createProject(String project) {
         // TODO Auto-generated method stub
-
+        logger.info("project : project :{}", project);
     }
 }
